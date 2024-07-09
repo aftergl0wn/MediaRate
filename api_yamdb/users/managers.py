ee85c87e9454c9a10be4c0c27b7db2740cb50ac3
Elizaveta Pokhlebkina <foxxybeat@gmail.com>
2024-07-09 22:43:14 +1100

    1

--- api_yamdb/users/managers.py
+++ api_yamdb/users/managers.py
@@ -25,5 +25,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(username, email, password,
-                                **extra_fields)
+        return self.create_user(username, email, password, **extra_fields)
