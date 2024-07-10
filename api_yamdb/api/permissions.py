4c1475285e791766e1f498b47b118b10269a3ed5
Elizaveta Pokhlebkina <foxxybeat@gmail.com>
2024-07-11 03:07:30 +1100

    исправила пермишены

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -29,8 +29,10 @@ class IsAdminOrSuperuser(permissions.BasePermission):
 class IsAdminOrReadOnlyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
+                or not request.user.is_authenticated
                 or request.user.role == 'admin')
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
+                or not request.user.is_authenticated
                 or request.user.role == 'admin')
