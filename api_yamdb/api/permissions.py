60e5f784eddfb17505db66386b16c2cf8f59ac3e
foxxybit <155355821+foxxybit@users.noreply.github.com>
2024-07-11 01:04:35 +1100

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,7 +4,6 @@ from rest_framework import permissions
 class IsAdminOrSuperuser(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        # Пропускаем анонимных пользователей
         if not request.user.is_authenticated:
             return False
         return request.user.role == 'admin' or request.user.is_superuser
